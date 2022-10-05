e644d6f396bd26c62d42b086787b839194990ee8
Sma-source <rabhi.smain@gmail.com>
2022-10-05 16:57:39 +0200

    import HeroBanner

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,13 @@
+import { Box } from "@mui/material";
 import React from "react";
+import HeroBanner from "../components/HeroBanner";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Box>
+      <HeroBanner />
+    </Box>
+  );
 };
 
 export default Home;
