453a98ec3ed5d1081f274a5f5c490568c5f45a38
Sma-source <rabhi.smain@gmail.com>
2022-10-06 15:13:01 +0200

    import & display  SearchExercise component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import { Box } from "@mui/material";
 import React from "react";
 import HeroBanner from "../components/HeroBanner";
+import SearchExercises from "../components/SearchExercises";
 
 const Home = () => {
   return (
     <Box>
       <HeroBanner />
+      <SearchExercises />
     </Box>
   );
 };
