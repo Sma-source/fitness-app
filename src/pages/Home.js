c0088870a692cba31f9d08d51b0ca7756bae9c4d
Sma-source <rabhi.smain@gmail.com>
2022-10-08 19:12:58 +0200

    import searchExercises

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,19 @@
 import { Box } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import HeroBanner from "../components/HeroBanner";
 import SearchExercises from "../components/SearchExercises";
 
 const Home = () => {
+  const [bodyPart, setbodyPart] = useState("all");
+  const [exercises, setExercises] = useState([]);
   return (
     <Box>
       <HeroBanner />
-      <SearchExercises />
+      <SearchExercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setbodyPart}
+      />
     </Box>
   );
 };
