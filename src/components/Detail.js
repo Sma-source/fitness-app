d1b8e640b8b0c03fa260e69bbfcff2ce601bd4b2
Sma-source <rabhi.smain@gmail.com>
2022-10-15 20:03:30 +0200

    init work fine

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Typography, Stack, Button } from "@mui/material";
+
 import BodyPartImage from "../assets/icons/body-part.png";
 import TargetImage from "../assets/icons/target.png";
 import EquipmentImage from "../assets/icons/equipment.png";
 
 const Detail = ({ exerciseDetail }) => {
   const { bodyPart, gifUrl, name, target, equipment } = exerciseDetail;
+
   const extraDetail = [
     {
       icon: BodyPartImage,
@@ -20,7 +22,7 @@ const Detail = ({ exerciseDetail }) => {
       name: equipment,
     },
   ];
-  console.log(extraDetail);
+
   return (
     <Stack
       gap="60px"
@@ -40,8 +42,8 @@ const Detail = ({ exerciseDetail }) => {
           color="#4F4C4C"
         >
           Exercises keep you strong.{" "}
-          <span style={{ textTransform: "capitalize" }}>{name}</span> is one of
-          the best <br /> exercises to target your {target}. It will help you
+          <span style={{ textTransform: "capitalize" }}>{name}</span> bup is one
+          of the best <br /> exercises to target your {target}. It will help you
           improve your <br /> mood and gain energy.
         </Typography>
         {extraDetail?.map((item) => (
--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -4,8 +4,50 @@ import { Box, Stack, Typography } from "@mui/material";
 
 import { exerciseOptions, fetchData } from "../utils/fetchData";
 import ExerciseCard from "./ExerciseCard";
+import Loader from "./Loader";
 
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [exercisesPerPage] = useState(6);
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      let exercisesData = [];
+
+      if (bodyPart === "all") {
+        exercisesData = await fetchData(
+          "https://exercisedb.p.rapidapi.com/exercises",
+          exerciseOptions
+        );
+      } else {
+        exercisesData = await fetchData(
+          `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`,
+          exerciseOptions
+        );
+      }
+
+      setExercises(exercisesData);
+    };
+
+    fetchExercisesData();
+  }, [bodyPart]);
+
+  // Pagination
+  const indexOfLastExercise = currentPage * exercisesPerPage;
+  const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
+  const currentExercises = exercises.slice(
+    indexOfFirstExercise,
+    indexOfLastExercise
+  );
+
+  const paginate = (event, value) => {
+    setCurrentPage(value);
+
+    window.scrollTo({ top: 1800, behavior: "smooth" });
+  };
+
+  if (!currentExercises.length) return <Loader />;
+
   return (
     <Box id="exercises" sx={{ mt: { lg: "109px" } }} mt="50px" p="20px">
       <Typography
@@ -22,9 +64,22 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
         flexWrap="wrap"
         justifyContent="center"
       >
-        {exercises.map((exercise, index) => {
-          return <ExerciseCard key={index} exercise={exercise} />;
-        })}
+        {currentExercises.map((exercise, idx) => (
+          <ExerciseCard key={idx} exercise={exercise} />
+        ))}
+      </Stack>
+      <Stack sx={{ mt: { lg: "114px", xs: "70px" } }} alignItems="center">
+        {exercises.length > 9 && (
+          <Pagination
+            color="standard"
+            shape="rounded"
+            defaultPage={1}
+            count={Math.ceil(exercises.length / exercisesPerPage)}
+            page={currentPage}
+            onChange={paginate}
+            size="large"
+          />
+        )}
       </Stack>
     </Box>
   );
